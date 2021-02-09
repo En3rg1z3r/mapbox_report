2afe4a16ceffb98ac56caeda7b2b671221a8d1fa
Feghouli Hamza <feghouli.hamza@gmail.com>
2021-02-09 14:15:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactMapGL from "react-map-gl";
+import {useState} from "react";
+
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 1500,
+    height: 900,
+    latitude: 35.369,
+    longitude: 1.330,
+    zoom: 12.43
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={"pk.eyJ1IjoiaGFtemFmZWdob3VsaSIsImEiOiJja2t2N2xmNmExMjE4MnVsbXlwcTNzdmc2In0.hacY17XkKR24Tr0Vs8PzDw"}
+      mapStyle="mapbox://styles/hamzafeghouli/ckkv7wgfo3pe417pgw0e1mycw"
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />     
     </div>
   );
 }
