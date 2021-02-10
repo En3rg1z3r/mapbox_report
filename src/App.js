b57cfe66c7451c6b9f58108e6282034a51940905
AliliMJ <alili.mohamedlamine@gmail.com>
2021-02-10 13:47:26 +0100

    Switching static map key to local key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import ReactMapGL from "react-map-gl";
-import {useState} from "react";
-
+import ReactMapGL from 'react-map-gl';
+import { useState } from 'react';
 
 function App() {
   const [viewport, setViewport] = useState({
     width: 1500,
     height: 900,
     latitude: 35.369,
-    longitude: 1.330,
-    zoom: 12.43
+    longitude: 1.33,
+    zoom: 12.43,
   });
-
 
   return (
     <div>
-     <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={"pk.eyJ1IjoiaGFtemFmZWdob3VsaSIsImEiOiJja2t2N2xmNmExMjE4MnVsbXlwcTNzdmc2In0.hacY17XkKR24Tr0Vs8PzDw"}
-      mapStyle="mapbox://styles/hamzafeghouli/ckkv7wgfo3pe417pgw0e1mycw"
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />     
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
+        mapStyle="mapbox://styles/hamzafeghouli/ckkv7wgfo3pe417pgw0e1mycw"
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
     </div>
   );
 }
