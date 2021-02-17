66979c5d8a5bb6114d7ce3ce649806f8e6fdf6c9
En3rg1z3r <feghouli.hamza@gmail.com>
2021-02-17 12:12:03 +0100

    EEEE

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,30 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+import ReactMapGL from "react-map-gl";
+import {useState} from "react";
+
+
+function App() {
+  const [viewport, setViewport] = useState({
+    width: 1500,
+    height: 900,
+    latitude: 35.369,
+    longitude: 1.330,
+    zoom: 12.43
+  });
+
+
+  return (
+    <div>
+     <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={"pk.eyJ1IjoiaGFtemFmZWdob3VsaSIsImEiOiJja2t2N2xmNmExMjE4MnVsbXlwcTNzdmc2In0.hacY17XkKR24Tr0Vs8PzDw"}
+      mapStyle="mapbox://styles/hamzafeghouli/ckkv7wgfo3pe417pgw0e1mycw"
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />     
+>>>>>>> 2afe4a16ceffb98ac56caeda7b2b671221a8d1fa
     </div>
   );
 }
